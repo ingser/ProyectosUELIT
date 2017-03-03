e3a56d0ae6248b3e62873981a89efa942f05a4dc
UELIT_joviedo <UELIT_joviedo@UGR-LEN-0003.praxisit.local>
2017-03-02 18:52:50 -0600

    Beans nuevo y configuración warning

---
+++ Arrendora/src/mx/isban/arrendadora/modelo/bean/DomicilioBean.java
@@ -0,0 +1,78 @@
+package mx.isban.arrendadora.modelo.bean;
+
+public class DomicilioBean {
+	
+	private String tipo_domicilio;
+	private String dom_extranjero;
+	private String calle;
+	private String num_ext;
+	private String num_int;
+	private String colonia;
+	private String cp;
+	private String cve_entidad;
+	private String entidad;
+	private String ciudad;
+	public String getTipo_domicilio() {
+		return tipo_domicilio;
+	}
+	public void setTipo_domicilio(String tipo_domicilio) {
+		this.tipo_domicilio = tipo_domicilio;
+	}
+	public String getDom_extranjero() {
+		return dom_extranjero;
+	}
+	public void setDom_extranjero(String dom_extranjero) {
+		this.dom_extranjero = dom_extranjero;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getNum_ext() {
+		return num_ext;
+	}
+	public void setNum_ext(String num_ext) {
+		this.num_ext = num_ext;
+	}
+	public String getNum_int() {
+		return num_int;
+	}
+	public void setNum_int(String num_int) {
+		this.num_int = num_int;
+	}
+	public String getColonia() {
+		return colonia;
+	}
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	public String getCve_entidad() {
+		return cve_entidad;
+	}
+	public void setCve_entidad(String cve_entidad) {
+		this.cve_entidad = cve_entidad;
+	}
+	public String getEntidad() {
+		return entidad;
+	}
+	public void setEntidad(String entidad) {
+		this.entidad = entidad;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	
+
+}
---
+++ Arrendora/src/mx/isban/arrendadora/modelo/bean/TelefonoFaxBean.java
@@ -0,0 +1,63 @@
+package mx.isban.arrendadora.modelo.bean;
+
+public class TelefonoFaxBean {
+
+	private String tel1;
+	private String ext1;
+	private String tel2;
+	private String ext2;
+	private String faxA;
+	private String extA;
+	private String faxB;
+	private String extB;
+	public String getTel1() {
+		return tel1;
+	}
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	public String getExt1() {
+		return ext1;
+	}
+	public void setExt1(String ext1) {
+		this.ext1 = ext1;
+	}
+	public String getTel2() {
+		return tel2;
+	}
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+	public String getExt2() {
+		return ext2;
+	}
+	public void setExt2(String ext2) {
+		this.ext2 = ext2;
+	}
+	public String getFaxA() {
+		return faxA;
+	}
+	public void setFaxA(String faxA) {
+		this.faxA = faxA;
+	}
+	public String getExtA() {
+		return extA;
+	}
+	public void setExtA(String extA) {
+		this.extA = extA;
+	}
+	public String getFaxB() {
+		return faxB;
+	}
+	public void setFaxB(String faxB) {
+		this.faxB = faxB;
+	}
+	public String getExtB() {
+		return extB;
+	}
+	public void setExtB(String extB) {
+		this.extB = extB;
+	}
+
+	
+}
